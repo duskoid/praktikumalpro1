1cb3063814dbfc5f16bb81cd1502aefa15e316b1
Rafi Putra Nugraha <19624017@mahasiswa.itb.ac.id>
2025-03-14 13:47:50 +0700

    Update AngkaPalindrom.c

--- AngkaPalindrom.c
+++ AngkaPalindrom.c
@@ -4,13 +4,13 @@ int main(){
     //kamus
     int n, sat, m, reversen=0; //n = angka input, m = n yang akan dikurangi angka satuannya setiap iterasi, sat = angka satuan, reversen = n tapi dari belakang
     scanf("%d", &n);
-    //assing m dengan nilai m untuk diambil satuannya
+    //assign m dengan nilai m untuk diambil satuannya
     m = n;
-
+    //reversing n
     while(m != 0){
-        sat = m % 10;
-        reversen = reversen*10 + sat;
-        m /= 10;
+        sat = m % 10; //ambili satuan dengan cari sisa pembagian 10
+        reversen = reversen*10 + sat; //taro angka satuan yang terakhir dibaca jadi puluhan, tambah dengan angka satuan yg baru diambil
+        m /= 10; //hilangkan angka satuan m
     }
     if (reversen == n){
         printf("1\n");
